ad2b82737ac00672c0f88664b1739e780fd4282d
leo <kuzmichleva1@gmail.com>
2026-04-12 18:40:52 +0200

    initial spring setup

---
+++ src/main/java/com/jexis/jexis_backend/JexisBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jexis.jexis_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JexisBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JexisBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jexis/jexis_backend/JexisBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jexis.jexis_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JexisBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
