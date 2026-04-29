dc90740b42f0d3a2c764544d09cbefdc41651225
leo <kuzmichleva1@gmail.com>
2026-04-29 18:24:55 +0200

    add route for getting all accounts

---
+++ src/main/java/com/jexis/jexis_backend/account/application/useCases/GetAccountsUseCase.java
@@ -0,0 +1,30 @@
+package com.jexis.jexis_backend.account.application.useCases;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.jexis.jexis_backend.account.domain.entities.Account;
+import com.jexis.jexis_backend.account.infrastructure.AccountRepository;
+
+/**
+ * GetAccountsUseCase
+ *
+ * This service class implements the use case for retrieving all existing
+ * accounts. It contains only the business logic related to fetching accounts,
+ * such as interacting with the repository to fetch the data.
+ *
+ * Author: Leo
+ */
+@Service
+public class GetAccountsUseCase {
+    AccountRepository repo;
+
+    public GetAccountsUseCase(AccountRepository repo) {
+        this.repo = repo;
+    }
+
+    public List<Account> execute() {
+        return repo.findAll();
+    }
+}
--- src/main/java/com/jexis/jexis_backend/account/presentation/AccountController.java
+++ src/main/java/com/jexis/jexis_backend/account/presentation/AccountController.java
@@ -1,8 +1,10 @@
 package com.jexis.jexis_backend.account.presentation;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.jexis.jexis_backend.account.application.dto.CreateAccountDto;
 import com.jexis.jexis_backend.account.application.useCases.CreateAccountUseCase;
 import com.jexis.jexis_backend.account.application.useCases.DeleteAccountUseCase;
+import com.jexis.jexis_backend.account.application.useCases.GetAccountsUseCase;
 import com.jexis.jexis_backend.account.domain.entities.Account;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -36,10 +39,31 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class AccountController {
     private final CreateAccountUseCase createAccountUseCase;
     private final DeleteAccountUseCase deleteAccountUseCase;
+    private final GetAccountsUseCase getAccountsUseCase;
 
-    public AccountController(CreateAccountUseCase createAccount, DeleteAccountUseCase deleteAccount) {
+    public AccountController(
+            CreateAccountUseCase createAccount,
+            DeleteAccountUseCase deleteAccount,
+            GetAccountsUseCase getAccounts) {
+
         this.createAccountUseCase = createAccount;
         this.deleteAccountUseCase = deleteAccount;
+        this.getAccountsUseCase = getAccounts;
+    }
+
+    /**
+     * Returns a list of all accounts.
+     *
+     * This endpoint retrieves all existing accounts by delegating to the
+     * getAccountsUseCase, which interacts with the repository to fetch the data.
+     *
+     * Endpoint: GET /account/list
+     *
+     * @return list of all accounts
+     */
+    @GetMapping("/list")
+    public List<Account> get() {
+        return getAccountsUseCase.execute();
     }
 
     /**
@@ -58,6 +82,17 @@ public class AccountController {
         return createAccountUseCase.execute(body);
     }
 
+    /**
+     * Handles account deletion requests.
+     *
+     * Accepts a {@link id} in the path, delegates execution to the
+     * deleteAccountUseCase, and returns the deleted {@link Account}.
+     *
+     * Endpoint: DELETE /account/delete/{id}
+     *
+     * @param id the ID of the account to delete
+     * @return message confirming deletion of the account with the specified ID
+     */
     @DeleteMapping("/delete/{id}")
     public String delete(@PathVariable UUID id) {
         deleteAccountUseCase.execute(id);
