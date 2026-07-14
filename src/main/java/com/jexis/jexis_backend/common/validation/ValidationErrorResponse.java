ff2ce220f1bdea3a9c47312102b4552f385030d4
Leo <kuzmichleva1@gmail.com>
2026-07-14 15:33:07 +0200

    remove unnecassary abtract error response class

--- src/main/java/com/jexis/jexis_backend/common/validation/ValidationErrorResponse.java
+++ src/main/java/com/jexis/jexis_backend/common/validation/ValidationErrorResponse.java
@@ -1,10 +1,10 @@
 package com.jexis.jexis_backend.common.validation;
 
-import com.jexis.jexis_backend.common.web.error.AErrorResponse;
+import com.jexis.jexis_backend.common.web.error.ErrorResponse;
 
 import java.util.List;
 
-public class ValidationErrorResponse extends AErrorResponse {
+public class ValidationErrorResponse extends ErrorResponse {
     private final List<ValidationError> errors;
 
     public ValidationErrorResponse(Integer status, String code, String message, List<ValidationError> errors) {
--- src/main/java/com/jexis/jexis_backend/common/web/error/ErrorResponse.java
+++ src/main/java/com/jexis/jexis_backend/common/web/error/ErrorResponse.java
@@ -2,15 +2,33 @@ package com.jexis.jexis_backend.common.web.error;
 
 /**
  * Strict returned error shape
- *
+ * <p>
  * This class defines the structure of error responses sent back to clients when
  * exceptions occur. It includes fields for an error code, a human-readable
  * message, and an HTTP status
- *
+ * <p>
  * Author: Leo
  */
-public class ErrorResponse extends AErrorResponse {
+public class ErrorResponse {
+    String code;
+    String message;
+    Integer status;
+
     public ErrorResponse(Integer status, String code, String message) {
-        super(status, code, message);
+        this.status = status;
+        this.code = code;
+        this.message = message;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
     }
 }
