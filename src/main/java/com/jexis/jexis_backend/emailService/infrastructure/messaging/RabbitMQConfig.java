03a82e4d26b7e33a22c786eb739fd0c5943c4587
Leo <kuzmichleva1@gmail.com>
2026-08-12 13:01:38 +0200

    Add PostgreSQL service in Docker, enhance CardRepository query, and refactor RabbitMQ message converter

--- src/main/java/com/jexis/jexis_backend/emailService/infrastructure/messaging/RabbitMQConfig.java
+++ src/main/java/com/jexis/jexis_backend/emailService/infrastructure/messaging/RabbitMQConfig.java
@@ -1,6 +1,7 @@
 package com.jexis.jexis_backend.emailService.infrastructure.messaging;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -29,6 +30,6 @@ public class RabbitMQConfig {
 
     @Bean
     public MessageConverter jsonMessageConverter() {
-        return new org.springframework.amqp.support.converter.Jackson2JsonMessageConverter();
+        return new JacksonJsonMessageConverter();
     }
 }
